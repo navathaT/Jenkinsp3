5449071cc392523ba26224eab7e939dd761fa6bf
Navatha <navathatula@gmail.com>
2025-07-24 09:09:29 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   description = "The name of the resource group to create."
   type        = string
-  default     = "rg-dev1"
+  default     = "rg-dev2"
 }
 
 variable "location" {
